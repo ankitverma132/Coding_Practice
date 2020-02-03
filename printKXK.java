7e5c6e20619d30e22a2f7ceb8768ea435e7e4575
Ankit Verma <57863091+ankitverma132@users.noreply.github.com>
2020-02-03 22:29:24 +0530

    Update printKXK.java

--- printKXK.java
+++ printKXK.java
@@ -1,3 +1,24 @@
+Given an n x n square matrix, print sum of all sub-squares of size k x k where k is smaller than or equal to n.
+See the sample input.
+Input format :
+Line 1 : Integer K
+Line 2 : Integer N
+Line 3 : NXN elements of the matrix separated by space
+Output Format :
+ Print all sums in a specific order
+Constraints :
+ 1 <= K <= 10^2
+ 1 <= N <= 10^3
+Sample Input :
+2
+3
+8 1 3
+2 9 3
+0 3 5
+Sample Output :
+20 16 
+14 20 
+
 package TwoDArray;
 import java.util.ArrayList;
 
